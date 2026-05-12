9f60fa91034f11275025a1fe741bb2f4ee2fa15f
sba24048 <sba24048@student.cct.ie>
2026-05-12 18:50:59 +0100

    Update uber.py

--- uber.py
+++ uber.py
@@ -17,5 +17,5 @@ def load_data(nrows):
 	data=pd.read_csv(DATA_URL, nrows = nrows)
 	lowercase = lambda x: str(x).lower()
 	data.rename(lowercase, axis=columns, inplace=True)
-	data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN]
+	data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
 	return data
