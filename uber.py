23fb971ea2a5080b987d3f7cd19e757ba2ee2315
sba24048 <sba24048@student.cct.ie>
2026-05-12 18:23:21 +0100

    Initial commit

---
+++ uber.py
@@ -0,0 +1,11 @@
+# Python App Creation as part of class practice for app creation.
+
+
+#libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# app creation
+st.title("Uber Pickups in NYC")
+
